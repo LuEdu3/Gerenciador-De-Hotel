1ae601f2df371e7c8c1eda727b9f133cee8672eb
Victor Emanuel <victorgit28@outlook.com>
2025-08-20 06:28:09 -0300

    Inserido conteudo na home de forma dinamica

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,39 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using GerenciadorHotel.Models;
+using GerenciadorHotel.Data;
 
 namespace GerenciadorHotel.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     [AllowAnonymous]
     public IActionResult Index()
     {
-        return View();
+        var acomodacoes = _context.Acomodacoes
+            .Where(a => a.Ativa && a.Status == StatusAcomodacao.Disponivel)
+            .Include(a => a.Imagens)
+            .OrderBy(a => a.Preco)
+            .Take(3)
+            .ToList();
+
+        ViewBag.TiposAcomodacao = _context.Acomodacoes
+            .Select(a => a.Nome)
+            .Distinct()
+            .ToList();
+
+        return View(acomodacoes);
     }
 
     [Authorize]
