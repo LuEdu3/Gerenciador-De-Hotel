a0fc8a0090dd58dd755f2738d3f9250193d0205d
Iolanda Carvalho <iolandacg2005@gmail.com>
2025-08-21 11:25:39 -0300

    Tentativa

--- Models/Acomodacao.cs
+++ Models/Acomodacao.cs
@@ -28,10 +28,19 @@ namespace GerenciadorHotel.Models
         [Display(Name = "Descrição")]
         public string? Descricao { get; set; }
 
-        [Required(ErrorMessage = "A quantidade de camas é obrigatória")]
-        [Range(1, 10, ErrorMessage = "A quantidade de camas deve ser entre 1 e 10")]
-        [Display(Name = "Quantidade de Camas")]
-        public int QuantidadeCamas { get; set; }
+
+    [Required(ErrorMessage = "A quantidade de camas é obrigatória")]
+    [Range(1, 10, ErrorMessage = "A quantidade de camas deve ser entre 1 e 10")]
+    [Display(Name = "Quantidade de Camas")]
+    public int QuantidadeCamas { get; set; }
+
+    [Range(0, 10, ErrorMessage = "A quantidade de camas de casal deve ser entre 0 e 10")]
+    [Display(Name = "Quantidade de Camas de Casal")]
+    public int QuantidadeCamasCasal { get; set; } = 0;
+
+    [NotMapped]
+    [Display(Name = "Quantidade de Camas de Solteiro")]
+    public int QuantidadeCamasSolteiro => QuantidadeCamas - QuantidadeCamasCasal;
 
         [Required(ErrorMessage = "O preço é obrigatório")]
         [Range(0.01, 9999.99, ErrorMessage = "O preço deve ser maior que zero")]
