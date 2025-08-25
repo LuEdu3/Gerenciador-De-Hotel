9dcba15e6f90a362a53375c2cb6f97a8b9ac1f79
Luiz Eduardo <Luizinformaticaeduardo@gmail.com>
2025-08-25 18:47:41 -0300

    Erro no banco de dados corrigido, porem sumiu as acomodações

---
+++ Data/Migrations/20250825213505_CorrigirColunasDuplicadas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorHotel.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrigirColunasDuplicadas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantidadeCamas",
+                table: "Acomodacoes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuantidadeCamas",
+                table: "Acomodacoes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
